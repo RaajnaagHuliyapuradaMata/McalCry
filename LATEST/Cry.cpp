59870d5c2f82b65a1c79d023145e2abc14497285
root <nagaraja.hm.0001@gmail.com>
2022-01-04 23:11:23 +0100

    Update: submodule Cry

---
+++ LATEST/Cry.cpp
@@ -0,0 +1,7 @@
+#include "Cry.h"
+
+class_Cry Cry;
+
+void class_Cry::InitFunction(void){
+}
+
---
+++ LATEST/Cry.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_Cry{
+   public:
+      void InitFunction(void);
+};
+
+extern class_Cry Cry;
+
