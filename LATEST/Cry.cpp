df0ae1d5d2128fe94ec501c52b261d4ec67e8d46
root <nagaraja.hm.0001@gmail.com>
2022-01-14 11:53:14 +0100

    Update: submodule Cry

--- LATEST/Cry.cpp
+++ LATEST/Cry.cpp
@@ -5,3 +5,57 @@ class_Cry Cry;
 FUNC(void, CRY_CODE) class_Cry::InitFunction(void){
 }
 
+FUNC(void, CRY_CODE) class_Cry::GetVersionInfo(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::ProcessJob(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::CancelJob(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyElementSet(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyValidSet(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeySetValid(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyElementGet(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyElementCopy(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyCopy(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyElementIdsGet(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::RandomSeed(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyGenerate(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyDerive(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyExchangeCalcPubVal(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::KeyExchangeCalcSecret(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::CertificateParse(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::CertificateVerify(void){
+}
+
+FUNC(void, CRY_CODE) class_Cry::MainFunction(void){
+}
+
--- LATEST/Cry.h
+++ LATEST/Cry.h
@@ -5,7 +5,25 @@
 
 class class_Cry{
    public:
-      FUNC(void, CRY_CODE) InitFunction(void);
+      FUNC(void, CRY_CODE) InitFunction          (void);
+      FUNC(void, CRY_CODE) GetVersionInfo        (void);
+      FUNC(void, CRY_CODE) ProcessJob            (void);
+      FUNC(void, CRY_CODE) CancelJob             (void);
+      FUNC(void, CRY_CODE) KeyElementSet         (void);
+      FUNC(void, CRY_CODE) KeyValidSet           (void);
+      FUNC(void, CRY_CODE) KeySetValid           (void);
+      FUNC(void, CRY_CODE) KeyElementGet         (void);
+      FUNC(void, CRY_CODE) KeyElementCopy        (void);
+      FUNC(void, CRY_CODE) KeyCopy               (void);
+      FUNC(void, CRY_CODE) KeyElementIdsGet      (void);
+      FUNC(void, CRY_CODE) RandomSeed            (void);
+      FUNC(void, CRY_CODE) KeyGenerate           (void);
+      FUNC(void, CRY_CODE) KeyDerive             (void);
+      FUNC(void, CRY_CODE) KeyExchangeCalcPubVal (void);
+      FUNC(void, CRY_CODE) KeyExchangeCalcSecret (void);
+      FUNC(void, CRY_CODE) CertificateParse      (void);
+      FUNC(void, CRY_CODE) CertificateVerify     (void);
+      FUNC(void, CRY_CODE) MainFunction          (void);
 };
 
 extern class_Cry Cry;
