2f065985409a39e6b6af068ce7c95dbb19deb0fe
root <nagaraja.hm.0001@gmail.com>
2022-02-25 12:06:58 +0100

    Update: submodule Cry

--- LATEST/Cry_EcuM.h
+++ LATEST/Cry_EcuM.h
@@ -27,8 +27,8 @@ class interface_Cry_EcuM : public interface_EcuM_Client{
 /*****************************************************/
 /* FUNCTIONS                                         */
 /*****************************************************/
-      FUNC(void, CRY_CODE) InitFunction   (void);
-      FUNC(void, CRY_CODE) DeInitFunction (void);
+      virtual FUNC(void, CRY_CODE) InitFunction   (void) = 0;
+      virtual FUNC(void, CRY_CODE) DeInitFunction (void) = 0;
 };
 
 /*****************************************************/
