02bed23acec7c5244094c66ba1d189e24d52041b
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-07 09:24:36 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Cry

--- LATEST/Cry.cpp
+++ LATEST/Cry.cpp
@@ -101,8 +101,8 @@ FUNC(void, CRY_CODE) module_Cry::InitFunction(
             lptrCfg = lptrCfgModule;
          }
          else{
-// use PBcfg_CanIf as back-up configuration
-            lptrCfg = PBcfg_CanIf;
+// use PBcfgCanIf as back-up configuration
+            lptrCfg = &PBcfgCry;
          }
       }
       IsInitDone = E_OK;
