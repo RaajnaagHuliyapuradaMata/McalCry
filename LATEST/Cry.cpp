bf1c2889529f1dc1d0381d7fcd3bd195df23573e
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-09 10:35:14 +0000

    Update: AUTOSAR basic software micro-controller abstraction driver module Cry

--- LATEST/Cry.cpp
+++ LATEST/Cry.cpp
@@ -35,6 +35,13 @@ class module_Cry:
       public abstract_module
    ,  public class_Cry_Functionality
 {
+   private:
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+            Std_TypeReturn          IsInitDone{E_NOT_OK};
+      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
+
    public:
       module_Cry(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
